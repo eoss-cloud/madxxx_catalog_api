cd072ca0f8da939e4a38b639d584cd24a9a35329
Thilo Wehrmann <thilo.wehrmann@eoss.cloud>
2016-10-13 15:02:21 +0200

    new root resource

--- catalog/client/services/root_service.py
+++ catalog/client/services/root_service.py
@@ -1,10 +1,23 @@
 import falcon
+import json
+
+from client.services.tools import can_zip_response, compress_body
+
+struct = {
+            'version':'v1',
+            'description':'EOSS catalog api'
+        }
 
 class RootResource(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.content_type = 'application/json'
-        resp.body = {
-            'version':'v1',
-            'description':'EOSS catalog api'
-        }
+
+        if can_zip_response(req.headers):
+            resp.set_header('Content-Type', 'application/json')
+            resp.set_header('Content-Encoding', 'gzip')
+            resp.body = compress_body(json.dumps(struct))
+        else:
+            resp.set_header('Content-Type', 'application/json')
+            resp.body = json.dumps(struct)
+
