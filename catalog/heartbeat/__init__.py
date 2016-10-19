36b50f15e4c0e883b3e80646cce0832c9e81e928
twehrmann <twehrmann@icloud.com>
2016-10-19 23:00:19 +0200

    add heartbeat func

--- catalog/heartbeat/__init__.py
+++ catalog/heartbeat/__init__.py
@@ -5,12 +5,18 @@ import sys
 import threading
 import time
 from threading import Thread
+import resource
 
 import os
 from decorator import decorate
 from general.catalog_logger import heartbeat_log, START, BEATING, STOP, STROKE, HEALTH, tracer_log, CALL
 
 import signal
+import datetime
+
+from heartbeat.messenger import Queue
+
+DO_LOGGING = False
 
 
 def signal_term_handler(signal, frame):
@@ -19,9 +25,26 @@ def signal_term_handler(signal, frame):
     sys.exit(0)
 
 
+def send_notification(logger, STATUS, message):
+    if isinstance(logger, logging.Logger) and DO_LOGGING:
+        logger.log(STATUS, message)
+    exec_time = datetime.datetime.now()
+
+    if logger.name == 'EOSS:heartbeat':
+        struct = dict()
+        struct['exec_time'] = exec_time.isoformat()
+        struct['status'] =  logging.getLevelName(STATUS)
+        struct['logger'] = logger.name
+        struct['message'] = message
+        q = Queue()
+        q.send(struct)
+
 
 def memory_usage_resource():
-    import resource
+    """
+
+    :return: kilobytes
+    """
     rusage_denom = 1024.
     if sys.platform == 'darwin':
         # ... it seems that in OSX the output is different units ...
@@ -35,9 +58,8 @@ def _trace(f, *args, **kw):
     username = pwd.getpwuid(os.getuid()).pw_name
     hostname = socket.gethostname().split(".")[0]
 
-
     kwstr = ', '.join('%r: %r' % (k, kw[k]) for k in sorted(kw))
-    tracer_log.log(CALL, "[%s:%s@%s]: %s (%s, {%s})" % (prog, username, hostname, f.__name__, args, kwstr))
+    send_notification(tracer_log, CALL, "[%s:%s@%s]: %s (%s, {%s})" % (prog, username, hostname, f.__name__, args, kwstr))
     return f(*args, **kw)
 
 
@@ -49,19 +71,18 @@ def lifesign(location, message, heartbeat_rate=2):
     t = threading.currentThread()
     while getattr(t, "do_run", True):
         time.sleep(heartbeat_rate)
-        heartbeat_log.log(BEATING, '(%s) func: <%s> is alive' % (location, message))
-
+        send_notification(heartbeat_log, BEATING, '(%s) func: <%s> is alive' % (location, message))
 
 
 def health(location, message, heartbeat_rate=2):
     start_time = time.time()
     t = threading.currentThread()
-    heartbeat_log.log(START, 'Starting health monitoring for <%s> (every %d secs.)' % (message, heartbeat_rate))
+    send_notification(heartbeat_log, START, 'Starting health monitoring for <%s> (every %d secs.)' % (message, heartbeat_rate))
     while getattr(t, "do_run", True):
         time.sleep(heartbeat_rate)
         l = os.getloadavg()
         m = memory_usage_resource()
-        heartbeat_log.log(HEALTH, '(%s) func: <%s>: %f, %f ' % (location, message, l[0], m))
+        send_notification(heartbeat_log, HEALTH, '(%s) func: <%s>: %f, %f ' % (location, message, l[0], m))
 
 
 def repeat(times, interval):
@@ -92,22 +113,27 @@ def _heartbeat(func, *args, **kwargs):
     health_thread.daemon = True
     health_thread.start()
     start_time = time.time()
-    heartbeat_log.log(START, '[%s] Starting heartbeat monitoring (every %d secs.)' % (lifesign_thread.name, heartbeat_rate_life))
+    send_notification(heartbeat_log, START, '[%s] Starting heartbeat monitoring (every %d secs.)' %
+                      (lifesign_thread.name, heartbeat_rate_life))
     try:
         retval = func(*args, **kwargs)
         lifesign_thread.do_run = False
         health_thread.do_run = False
-        heartbeat_log.log(STOP, "[%s] Stopping heartbeat monitoring successfully. (exec. time: %f secs.)." % (lifesign_thread.name, time.time() - start_time))
+        send_notification(heartbeat_log, STOP, "[%s] Stopping heartbeat monitoring successfully. (exec. time: %f secs.)." %
+                          (lifesign_thread.name, time.time() - start_time))
     except (KeyboardInterrupt, SystemExit), e:
-        heartbeat_log.log(STOP, "[%s] Stopping heartbeat monitoring gracefully (exec. time: %f secs.)." % (lifesign_thread.name, time.time() - start_time))
+        send_notification(heartbeat_log, STOP, "[%s] Stopping heartbeat monitoring gracefully (exec. time: %f secs.)." %
+                          (lifesign_thread.name, time.time() - start_time))
+
         time.sleep(1)
         lifesign_thread.do_run = False
         health_thread.do_run = False
         return None
     except Exception, e:
         heartbeat_log.log(STROKE, e)
+        send_notification(heartbeat_log, STROKE, e)
         heartbeat_log.exception('Error occured during heartbeat monitored execution')
-        heartbeat_log.log(STOP, "[%s] Stopping heartbeat monitoring with error (exec. time: %f secs.)." % (lifesign_thread.name, time.time() - start_time))
+        send_notification(heartbeat_log, STOP, "[%s] Stopping heartbeat monitoring with error (exec. time: %f secs.)." % (lifesign_thread.name, time.time() - start_time))
         lifesign_thread.do_run = False
         health_thread.do_run = False
         return None
---
+++ catalog/heartbeat/messenger.py
@@ -0,0 +1,56 @@
+import boto3
+import ujson
+
+MAX_MESSAGES = 10
+
+
+class Queue(object):
+    def __init__(self):
+        self.sqs = boto3.resource('sqs')
+        self.queue_name = 'EOSS_notifications'
+
+    def __len__(self):
+        queue = self.sqs.get_queue_by_name(QueueName=self.queue_name)
+        return int(queue.attributes.get('ApproximateNumberOfMessages'))
+
+
+    def __create_queue__(self):
+        # Create the queue. This returns an SQS.Queue instance
+        queue = self.sqs.create_queue(QueueName=self.queue_name, Attributes={'DelaySeconds': '5'})
+
+        # You can now access identifiers and attributes
+        print(queue.url)
+        print(queue.attributes.get('DelaySeconds'))
+
+    def send(self, body):
+        # Get the queue
+        queue = self.sqs.get_queue_by_name(QueueName=self.queue_name)
+
+        # Create a new message
+        response = queue.send_message(MessageBody=ujson.dumps(body))
+        #print response
+
+
+    def get_messages(self):
+        # Get the queue
+        queue = self.sqs.get_queue_by_name(QueueName=self.queue_name)
+
+        # Process messages by printing out body and optional author name
+        for message in queue.receive_messages(MaxNumberOfMessages=MAX_MESSAGES, WaitTimeSeconds=10, VisibilityTimeout=60,):
+            # Get the custom author message attribute if it was set
+
+            # Print out the body and author (if set)
+
+
+            # Let the queue know that the message is processed
+            #message.delete()
+            yield message
+
+    def delete_message(self, message):
+        queue = self.sqs.get_queue_by_name(QueueName=self.queue_name)
+        queue.delete_messages(Entries=message)
+
+
+if __name__ == '__main__':
+    q = Queue()
+    print q.get_messages()
